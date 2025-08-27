6ccfb547347d850cf70061bf5cdc3130967fc92c
Tristan Carter <91004717+tristan-carter@users.noreply.github.com>
2025-08-27 22:16:40 +0100

    Update Piece.cpp comments

--- Piece.cpp
+++ Piece.cpp
@@ -15,20 +15,10 @@ Piece::~Piece() {
 
 void Piece::find_straight_moves(BoardStructure& board, Coord piece_position,
                                 short int player_move_multiplier, bool is_king, bool check_for_pin) {
-    // Step 1 - iterates through each possible direction the piece could
-    // move (up, down, left, right)
     for (int direction = 1; direction < 5; direction++) {
-        // Step 2 - iterates from 1 to the furthest the piece could move
-        // in one straight direction (one less than the board size), if
-        // an obstacle is reached then this for loop will be broken out
-        // of so you cannot jump over other pieces or go off the end off
-        // the end of the board
         for (int move_by = 1; move_by < BOARD_SIZE; move_by++)
         {
             bool is_direction_cutoff = false;
-            // Step 3 - assigns the move_to coordinate to be investigated
-            // a value for each iteration depending on the direction and
-            // the value of move_by
             Coord move_to{};
             if (direction == 1) {
                 // up
@@ -51,35 +41,19 @@ void Piece::find_straight_moves(BoardStructure& board, Coord piece_position,
                 move_to.y = piece_position.y;
             }
 
-            // Step 4 - validates that the coordinate to move to is a valid coordinate on the board
-            // and that if the piece moving is a king that the piece is only moving by one square
             if (this->is_coord_in_board(move_to)
                 && (is_king == false || move_by == 1) /*(King's can only move one square)*/
                 && is_direction_cutoff == false) {
 
-                // Step 5 - initiates a variable to keep track of the cell which the piece
-                // is moving to so that code is repeated less. Then checks that if there
-                // is a piece on the cell to move to that it belongs to the opposition
                 BoardCell& move_to_cell{ board.board[move_to.x][move_to.y] };
 
                 if (move_to_cell.piece == nullptr || (move_to_cell.piece != nullptr
                     && move_to_cell.piece->player_side != board.board[piece_position.x][piece_position.y].piece->player_side)) {
-
-                    // Step 6 - checks if the board cell to move to has a piece on it, we already
-                    // know this piece would have to be on the opposition's side due to the checks
-                    // completed in step 5 so don't need to check for which side the piece is on.
-                    // If there is a piece on the cell to move to then a possible move is added
-                    // with the coordinate the piece is moving to being passed as the  piece_taken
-                    // coordinate parameter to the add_possible_move method. If there is no piece
-                    // on the board cell to move to (so no piece will be taken) then the
-                    // add_possible_move method is called with the same parameters except a
-                    // coordinate of (-1, 0) is passed as the piece_taken coordinate parameter,
-                    // this coordinate is not on the board and represents that no piece is to be taken
+                    
                     if (move_to_cell.piece != nullptr) {
                         board.add_possible_move(move_to, piece_position, move_to, check_for_pin);
                         is_direction_cutoff = true;
                         break;
-
                     }
                     else {
                         board.add_possible_move(move_to, piece_position, Coord{ -1, 0 }, check_for_pin);
@@ -94,69 +68,42 @@ void Piece::find_straight_moves(BoardStructure& board, Coord piece_position,
 
 void Piece::find_diagonal_moves(BoardStructure& board, Coord piece_position,
                                 short int player_move_multiplier, bool is_king, bool check_for_pin) {
-    // Step 1 - iterates through each possible direction the piece could
-    // move (top-right, top-left, bottom-right, bottom-left)
     for (int direction = 1; direction < 5; direction++) {
-        // Step 2 - iterates from 1 to the furthest the piece could move
-        // in one straight direction (one less than the board size), if
-        // an obstacle is reached then this for loop will be broken out
-        // of so you cannot jump over other pieces or go off the end off
-        // the end of the board
         for (int move_by = 1; move_by < BOARD_SIZE; move_by++)
         {
-            // Step 3 - assigns the move_to coordinate to be investigated
-            // a value for each iteration depending on the direction and
-            // the value of move_by
             Coord move_to{};
             if (direction == 1) {
-                // top right
+                // top right diagonal
                 move_to.x = piece_position.x + move_by;
                 move_to.y = piece_position.y + move_by;
             }
             else if (direction == 2) {
-                // top left
+                // top left diagonal
                 move_to.x = piece_position.x - move_by;
                 move_to.y = piece_position.y + move_by;
             }
             else if (direction == 3) {
-                // bottom right
+                // bottom right diagonal
                 move_to.x = piece_position.x + move_by;
                 move_to.y = piece_position.y - move_by;
             }
             else if (direction == 4) {
-                // bottom left
+                // bottom left diagonal
                 move_to.x = piece_position.x - move_by;
                 move_to.y = piece_position.y - move_by;
             }
 
-            // Step 4 - validates that the coordinate to move to is a valid coordinate on the board
-            // and that if the piece moving is a king that the piece is only moving by one square
             if (this->is_coord_in_board(move_to)
                 && (!is_king || move_by == 1) /*(King's can only move one square)*/) {
 
-                // Step 5 - initiates a variable to keep track of the cell which the piece
-                // is moving to so that code is repeated less. Then checks that if there
-                // is a piece on the cell to move to that it belongs to the opposition
                 BoardCell& move_to_cell{ board.board[move_to.x][move_to.y] };
 
                 if (move_to_cell.piece == nullptr || (move_to_cell.piece != nullptr
-                                                      && move_to_cell.piece->player_side !=
-                                                             board.board[piece_position.x][piece_position.y].piece->player_side)) {
+                    && move_to_cell.piece->player_side != board.board[piece_position.x][piece_position.y].piece->player_side)) {
 
-                    // Step 6 - checks if the board cell to move to has a piece on it, we already
-                    // know this piece would have to be on the opposition's side due to the checks
-                    // completed in step 5 so don't need to check for which side the piece is on.
-                    // If there is a piece on the cell to move to then a possible move is added
-                    // with the coordinate the piece is moving to being passed as the  piece_taken
-                    // coordinate parameter to the add_possible_move method. If there is no piece
-                    // on the board cell to move to (so no piece will be taken) then the
-                    // add_possible_move method is called with the same parameters except a
-                    // coordinate of (-1, 0) is passed as the piece_taken coordinate parameter,
-                    // this coordinate is not on the board and represents that no piece is to be taken
                     if (move_to_cell.piece != nullptr) {
                         board.add_possible_move(move_to, piece_position, move_to, check_for_pin);
                         break;
-
                     }
                     else {
                         board.add_possible_move(move_to, piece_position, Coord{ -1, 0 }, check_for_pin);
@@ -171,7 +118,6 @@ void Piece::find_diagonal_moves(BoardStructure& board, Coord piece_position,
 
 bool Piece::is_coord_in_board(Coord coord)
 {
-    // Validates that a coordinate is within the board
     return (coord.x > -1
             && coord.x < BOARD_SIZE
             && coord.y > -1
